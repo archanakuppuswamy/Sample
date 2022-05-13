418cfd3f6dd5281ccc3516ea48d8392d45fb1673
SuRArC <SuRArC@SURYA------>
2022-05-13 14:46:10 +0530

    sample test pushed

---
+++ SampleTest/src/test/java/com/FrameWorkSample/CreateContact.java
@@ -0,0 +1,13 @@
+package com.FrameWorkSample;
+
+import org.testng.annotations.Test;
+
+public class CreateContact {
+	@Test
+
+	public void createContactTest() {
+		System.out.println("execute create Contact test");
+	}
+	
+
+}
---
+++ SampleTest/src/test/java/com/FrameWorkSample/OrganizationTest.java
@@ -0,0 +1,12 @@
+package com.FrameWorkSample;
+
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+	@Test
+	public void createOrgTest() {
+		System.out.println("execute create org test");
+	}
+	
+
+}
